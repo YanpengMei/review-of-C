81060e205e69785cd2b7fe7c71a2a23823e3f82e
Yanpeng Mei <95927831+YanpengMei@users.noreply.github.com>
2022-02-15 22:33:40 +0100

    Update linspace10.h

--- Code/linspace10.h
+++ Code/linspace10.h
@@ -10,3 +10,24 @@ double* linspace10(double x1, double x2)
 	}
 	return linsp_10;
 }
+
+
+/* test of 'linspace10'
+#include <stdio.h>
+#include "linspace10.h"
+int main()
+{
+double x1 = 0;
+	double x2 = 1;
+	int i = 0;
+	double *t1;
+
+	t1 = linspace10(x1, x2);
+	for (i = 0; i < 10; i++)
+	{
+		printf("*(t1 + %d) : %f\n", i, *(t1 + i));
+	}
+	*/
+	return 0;
+}
+*/
